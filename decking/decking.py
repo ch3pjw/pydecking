37d8e2cb3bdd15529791cc86342902b4c9b28dec
Paul Weaver <paul@ruthorn.co.uk>
2014-07-01 16:09:47 +0100

    Add initial decking code to run up containers

---
+++ decking/decking.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import time
+import yaml
+import docker
+
+
+class DeckingRunner(object):
+    '''Takes decking configuration, as defined in the decking project and
+    runs it using the python docker API.
+
+    :parameter decking_config: Python structure containing the parsed
+        decking.json file config
+
+    All extra kwargs are passed to the docker python client.
+    '''
+    def __init__(self, decking_config, docker_client):
+        self.decking_config = decking_config
+        self.client = docker_client
+
+    @staticmethod
+    def _build_volume_binding(mount_spec):
+        mount_point, source = mount_spec.split(':', 2)
+        return mount_point, {'bind': source, 'ro': False}
+
+    @staticmethod
+    def _uncolon_mapping(mapping_as_sequence):
+        return dict(item.split(':') for item in mapping_as_sequence)
+
+    def _containter_names_by_dependency(self, container_specs):
+        to_process = set(container_specs.keys())
+        processed = set()
+        while to_process:
+            pending = set()
+            for name in list(to_process):
+                dependencies = container_specs[name].get('dependencies', [])
+                links = self._uncolon_mapping(dependencies)
+                if all(link in processed for link in links):
+                    to_process.remove(name)
+                    pending.add(name)
+                    yield name
+            if pending:
+                yield None
+            else:
+                # Stuck not able to process any more containers
+                raise RuntimeError('Arg, you have bad dependencies')
+            processed |= pending
+
+    def create_container(self, container_spec, name):
+        image = container_spec['image']
+        environment = container_spec.get('env', [])
+        port_bindings = self._uncolon_mapping(container_spec.get('port', []))
+        print('creating container {!r}... '.format(name), end='')
+        container_info = self.client.create_container(
+            image,
+            name=name,
+            environment=environment,
+            ports=port_bindings.keys())
+        print('({})'.format(container_info['Id'][:12]))
+        # FIXME: make this less side-effecty?
+        container_spec['instance'] = container_info
+        return container_info
+
+    def run_container(self, container_spec, name):
+        dependencies = container_spec.get('dependencies', [])
+        links = self._uncolon_mapping(dependencies)
+        volume_bindings = dict(
+            self._build_volume_binding(mount_entry)
+            for mount_entry in container_spec.get('mount', []))
+        port_bindings = self._uncolon_mapping(container_spec.get('port', []))
+        if 'instance' not in container_spec:
+            raise RuntimeError(
+                'Must create a container instance before attmepting to run')
+        print('running container {!r} ({})...'.format(
+            name, container_spec['instance']['Id'][:12]))
+        self.client.start(
+            container_spec['instance'],
+            binds=volume_bindings,
+            links=links,
+            port_bindings=port_bindings)
+
+    def create_all(self):
+        container_specs = self.decking_config['containers']
+        created = []
+        for name in self._containter_names_by_dependency(container_specs):
+            if name:
+                container_spec = container_specs[name]
+                self.create_container(container_spec, name)
+                created.append(name)
+        return created
+
+    def run_all(self):
+        container_specs = self.decking_config['containers']
+        running = []
+        for name in self._containter_names_by_dependency(container_specs):
+            if name:
+                container_spec = container_specs[name]
+                self.run_container(container_spec, name)
+                running.append(name)
+            else:
+                # FIXME: make some way to tell if container is alive
+                time.sleep(6)
+        return running
+
+
+if __name__ == '__main__':
+    with open('decking.json') as f:
+        decking_config = yaml.load(f)
+
+    docker_client = docker.Client(
+        base_url='unix://var/run/docker.sock', version='0.9.1', timeout=10)
+    runner = DeckingRunner(decking_config, docker_client)
+    runner.create_all()
+    runner.run_all()
---
+++ decking/test_decking.py
@@ -0,0 +1,79 @@
+from unittest2 import TestCase
+from mock import MagicMock, patch
+import docker
+
+from decking import DeckingRunner
+
+
+@patch('time.sleep')
+class TestDeckingRunner(TestCase):
+    def setUp(self):
+        self.decking_config = {
+            'containers': {
+                'alice': {
+                    'image': 'repo/alice',
+                    'dependencies': ['bob:bob_alias'],
+                    'port': ['1234:2345']
+                },
+                'bob': {
+                    'image': 'repo/bob',
+                    'env': ['A=b']
+                }
+            }
+        }
+        self.mock_docker_client = MagicMock(spec=docker.Client, instance=True)
+        self.container_info_1 = {'Id': 'abcd1234'}
+        self.container_info_2 = {'Id': 'efab5678'}
+
+    def test_uncolon_mapping(self, sleep_mock):
+        self.assertEqual(
+            DeckingRunner._uncolon_mapping(['a:b', 'c:d']),
+            {'a': 'b', 'c': 'd'})
+
+    def test_create_all(self, sleep_mock):
+        runner = DeckingRunner(self.decking_config, self.mock_docker_client)
+        self.mock_docker_client.create_container.side_effect = [
+            self.container_info_1, self.container_info_2]
+        created = runner.create_all()
+        self.assertEqual(created, ['bob', 'alice'])
+        self.assertEqual(
+            self.decking_config['containers']['bob']['instance'],
+            self.container_info_1)
+        self.assertEqual(
+            self.decking_config['containers']['alice']['instance'],
+            self.container_info_2)
+
+    def test_run_all(self, sleep_mock):
+        runner = DeckingRunner(self.decking_config, self.mock_docker_client)
+        self.assertRaisesRegexp(RuntimeError, 'Must create', runner.run_all)
+        self.decking_config['containers']['bob']['instance'] = (
+            self.container_info_1)
+        self.decking_config['containers']['alice']['instance'] = (
+            self.container_info_2)
+        runner.run_all()
+
+    def test_bad_depenencies(self, sleep_mock):
+        decking_config = {
+            'containers': {
+                'zen': {
+                    'image': 'repo/zen',
+                    'dependencies': ['zen:zen_alias']
+                }
+            }
+        }
+        runner = DeckingRunner(decking_config, self.mock_docker_client)
+        self.assertRaisesRegexp(RuntimeError, 'dependencies', runner.run_all)
+        decking_config = {
+            'containers': {
+                'zen': {
+                    'image': 'repo/zen',
+                    'dependencies': ['yen:yen_alias']
+                },
+                'yen': {
+                    'image': 'repo/yen',
+                    'dependencies': ['zen:zen_alias']
+                }
+            }
+        }
+        runner = DeckingRunner(decking_config, self.mock_docker_client)
+        self.assertRaisesRegexp(RuntimeError, 'dependencies', runner.run_all)

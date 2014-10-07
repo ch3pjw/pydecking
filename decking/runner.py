24066c67a161a7d8aae5228ae60605be000bbc0f
Paul Weaver <paul@ruthorn.co.uk>
2014-10-07 10:33:29 +0100

    Make Queue import Python3 compatible

--- decking/runner.py
+++ decking/runner.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
-from Queue import Queue
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
 import threading
 import sys
 import signal
